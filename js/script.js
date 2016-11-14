e173b5135dfbe35c23cdd8ea9a110d1589f00597
Grzegorz Stańczyk <grzegorz.stanczyk@o2.pl>
2016-11-14 22:52:12 +0100

    Zadanie 4

--- js/script.js
+++ js/script.js
@@ -10,26 +10,56 @@
 		return this.addClass('color')
 	}
 
+	$.fn.title = function() {
+		return this.addClass('title')
+	}
+
 
 	$('.grid.grid-12').bgc()
 	$('.nav a[href^=http]').bgc()
 	$('input:radio, input:checkbox').not(':checked').parent().color()
 	$('#text p:empty:first').remove()
 	$('.pagination-item').not('span').color()
+	$('#container > p').title()
 
 
 	// Zadanie 3
 	var $hamburger = $('.hamburger')
-  	$hamburger.click(function() {
+	var $nav = $('nav')
 
+  	$hamburger.click(function() {
     	var $this = $(this)
    		$this.toggleClass('toggle')
   	})
 
-  	var $nav = $('nav')
+
 
   	$hamburger.on('click', function() {
   		$nav.toggleClass('open')
   	})
+
+
+  	// Zadanie 4
+  	function createRegist($registr) {
+  		return "<li>" + $registr + "</li>"
+  	}
+
+  	var $form = $('.form')
+  	var $input = $("input[name='registr']")
+  	var $list = $('<ul/>').insertAfter($form)
+
+  	$form.on('submit', function(e) {
+  		e.preventDefault()
+
+  		var $registr = $.trim($input.val())
+
+  		if ($registr === '') {
+  			return
+  		}
+
+  		$list.append(createRegist($registr))
+
+  	})
+
 
 })(jQuery)
